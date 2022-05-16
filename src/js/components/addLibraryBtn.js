3e8ee1c88d00a67335288f178db8024e7b7b9f27
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-05-16 10:42:03 +0300

    Update addLibraryBtn.js

--- src/js/components/addLibraryBtn.js
+++ src/js/components/addLibraryBtn.js
@@ -5,6 +5,7 @@ const LOCALSTORAGE_KEY_Watched = 'WatchedLibrary';
 const LOCALSTORAGE_KEY_Queue = 'QueueLibrary';
 
 export function myLibrary(dataMovie) {
+  console.log(dataMovie);
   const btnRefs = {
     addToWatchedBtn: document.querySelector('.add-watch'),
     addToQueueBtn: document.querySelector('.add-queue'),
